8ed45e3e6a763a123e226711de19b6d17b1c8c5b
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-19 10:21:39 +0000

    4-27

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "homes#top"
+  root to: "homes#top", as: "top"
   
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   
   get "/homes/about" => "homes#about", as: "about"
   
