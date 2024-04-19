9cb49bcde8d4d3c15b83f2af8f95b5c2379b3bc1
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-19 15:24:49 +0000

    4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,17 +7,22 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
-    @book = Book.new
     @books = Book.all
+    @book = Book.new
+    @user=current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @user=current_user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  
+
   def index
     @users = User.all
+    @book=Book.new
+    @user=current_user
   end
 
   def show
@@ -12,11 +14,11 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
     user.update(user_params)
-    redirect_to user_path(user.id) 
+    redirect_to user_path(user.id)
   end
 
 
@@ -25,5 +27,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
 end
