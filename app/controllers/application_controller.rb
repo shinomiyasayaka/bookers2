49563db2b2c841bd2e994de464bc2c95fe7daa80
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-20 10:39:58 +0000

    4-27

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  
   def after_sign_in_path_for(resource)
     flash[:notice] = "Signed in successfully."
     user_path(current_user.id)
@@ -12,6 +13,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     flash[:notice] = "Signed out successfully."
     top_path
+    
   end
 
   protected
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
     @users = User.all
@@ -30,7 +32,14 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
