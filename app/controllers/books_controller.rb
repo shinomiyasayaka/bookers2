1e4b91e43b21fa94f18bdf5232dd3e02d55d1af8
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-20 12:57:01 +0000

    4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,12 +22,12 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
-    @user=current_user
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
-    @user=current_user
+    @user = @book.user
   end
 
   def edit
