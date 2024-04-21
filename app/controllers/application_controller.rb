2749a1651b6d7ca675c3c53d591b1c6fb6b510c5
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-21 13:02:54 +0000

    4-27

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
-  
-  before_action :authenticate_user!, except: [:top]
-  
+
+  before_action :authenticate_user!, except: [:top, :about]
+
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  
+
+
   def after_sign_in_path_for(resource)
     flash[:notice] = "Signed in successfully."
     user_path(current_user.id)
   end
-  
+
   def after_sign_out_path_for(resource)
     flash[:notice] = "Signed out successfully."
     top_path
-    
+
   end
 
   protected
@@ -21,5 +21,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
   end
-  
+
 end
