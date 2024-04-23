4ebb053c8b0e81e00ef4611f210a2fd724afb0c5
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-23 07:00:43 +0000

    4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
   before_action :ensure_correct_user, only: [:edit, :update]
 
   def new
@@ -58,7 +58,7 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
   def ensure_correct_user
     book = Book.find(params[:id])
     unless book.user_id == current_user.id
